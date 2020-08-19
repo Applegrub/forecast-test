7044ed9c6ec50f5d0d22faf8ff3396dcfd72ea66
Applegrub <sidyukovmichail@list.ru>
2020-08-20 00:50:22 +0300

    Updates

--- precache-manifest.1f4f586204287a3a8da0ae92040dbc37.js
+++ precache-manifest.1f4f586204287a3a8da0ae92040dbc37.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8c803c8abc37555ab13b282fe878f8b",
+    "revision": "052bee25c1486131792ccd0a7c6fb8a0",
     "url": "/forecast-test/index.html"
   },
   {
-    "revision": "50c8c6d497b710f9e95f",
+    "revision": "73b091948493bb0f8f9f",
     "url": "/forecast-test/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/forecast-test/static/js/2.59a1595d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "50c8c6d497b710f9e95f",
-    "url": "/forecast-test/static/js/main.ad20aea1.chunk.js"
+    "revision": "73b091948493bb0f8f9f",
+    "url": "/forecast-test/static/js/main.b1326875.chunk.js"
   },
   {
     "revision": "9dc2429e0e7fd8d44e4b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/forecast-test/precache-manifest.f57488c35ed5969fa0955320aacefd02.js"
+  "/forecast-test/precache-manifest.1f4f586204287a3a8da0ae92040dbc37.js"
 );
 
 self.addEventListener('message', (event) => {
